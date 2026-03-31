f8d647f0d7ab6ae50625bca620b3fa015ea871b9
aleksanderZak7 <aleksander.zak77@gmail.com>
2026-03-31 13:41:27 +0200

    Refactor CLI and processing pipeline

--- src/algorithms/runner.py
+++ src/algorithms/runner.py
@@ -1,84 +1,101 @@
-import os
+import json
+import timeit
 import imageio
-
-from datetime import datetime
+import pydicom
+import numpy as np
+from typing import Any
+from pathlib import Path
+from datetime import timedelta
+from skimage.transform import resize
 
 import src.arguments as ah
-
-from src.algorithms.unsharping_mask import UM
-from src.algorithms.clahe import CLAHE
 from src.algorithms.hef import HEF
+from src.algorithms.clahe import CLAHE
+from src.algorithms.unsharping_mask import UM
+
 
 class AlgorithmRunner:
-	def __init__(self):
-		self.arg_handler	= ah.ArgumentHandler()
-		self.algorithm		= self.arg_handler.get_algorithm()
-		self.image				= self.arg_handler.get_image()
-		self.images_path	= self.arg_handler.get_path()
-		output_path = self.arg_handler.get_output_path()
-		if output_path:
-			self.results_path = output_path
+
+	__slots__ = ("_negate", "_algorithm", "_out_shape", "_images_path", "_results_path", "_params")
+
+	def __init__(self) -> None:
+		arg_handler = ah.ArgumentHandler()
+		self._negate: bool = arg_handler.get_negate
+		self._algorithm: str = arg_handler.get_algorithm
+		self._out_shape: int | None = arg_handler.get_shape
+		self._params: dict[str, Any] = arg_handler.get_params
+  
+		self._images_path: Path = arg_handler.get_input_path
+		self._results_path: Path = arg_handler.get_output_path
+
+		self._results_path.mkdir(parents=True, exist_ok=True)
+
+
+	def run(self) -> None:
+		"""Runs the selected algorithm for the *.dcm files in provided path."""
+		self._print_run_info()
+		start: float = timeit.default_timer()
+		alg: UM | HEF | CLAHE = self._select_algorithm()
+
+		for image_path in self._images_path.glob("*.dcm"):
+			dataset: pydicom.FileDataset = pydicom.dcmread(image_path)
+
+			pixel_matrix: np.ndarray = dataset.pixel_array
+			if self._negate:
+				pixel_matrix = np.max(pixel_matrix) - pixel_matrix
+
+			processed_image = alg.run(pixel_matrix)
+
+			if self._out_shape is not None:
+				processed_image = (resize(processed_image, (self._out_shape, self._out_shape)) * 255).astype(np.uint8) # type: ignore
+			imageio.imwrite(self._results_path / f"{image_path.stem}.png", processed_image)
+
+		stop: float = timeit.default_timer()
+		time: float = stop - start
+		self._export_run_info(time)
+		print(f"Processing complete. Output saved to {self._results_path}. Runtime: {timedelta(seconds=time)}")
+
+
+	def _print_run_info(self) -> None:
+		"""Prints the information about the current run, including the selected algorithm, parameters, and output settings."""
+		print('\n' + '=' * 10 + " RUN INFO " + '=' * 10)
+		print(f"Algorithm: {self._algorithm.upper()}")
+		print(f"Params:")
+		for key, value in self._params.items():
+			if value is not None:
+				print(f"\t{key}: {value}")
+
+		print(f"Negate: {self._negate}")
+		if self._out_shape is not None:
+			print(f"Output shape: {self._out_shape}x{self._out_shape}")
+		print('=' * 30 + '\n')
+
+
+	def _select_algorithm(self) -> UM | HEF | CLAHE:
+		"""Selects the algorithm to be used based on the provided argument.
+
+		Raises:
+			ValueError: If the provided algorithm is not supported.
+
+		Returns:
+			UM | HEF | CLAHE: An instance of the selected algorithm class.
+		"""
+		if self._algorithm == 'um':
+			return UM(self._params["filter_type"], self._params["amount"], self._params["radius"])
+
+		elif self._algorithm == 'hef':
+			return HEF(self._params["d0"])
+
+		elif self._algorithm == 'clahe':
+			return CLAHE(self._results_path, self._params["window_size"], 
+                self._params["clip_limit"], self._params["n_iter"], self._params["log"])
+
 		else:
-			self.results_path	= os.path.join("results", str(datetime.now()))
+			raise ValueError(f"Algorithm {self._algorithm} is not supported!")
 
-		os.makedirs(self.results_path, exist_ok=True)
 
-	def __del__(self):
-		self.algorithm		= ''
-		self.image				= ''
-		self.images_path	= ''
-		self.results_path	= ''
-
-	def run(self):
-		'''Runs the algorithm in the images.'''
-
-		if self.images_path:
-			images = os.listdir(self.images_path)
-			path = self.images_path
-		else:
-			# We put in a list to be able to utilize the for loop
-			images = [self.image]
-			path = ""
-
-		for image in images:
-			split_image = image.split('/')
-			if len(split_image) != 1:
-				self.image = split_image[-1]
-			else:
-				self.image = image
-
-			processed_image = self.__run_algorithm(image, path)
-			t = datetime.now()
-			name = self.image.split(".")[0]
-			filename = f"{t.hour}_{t.minute}_{t.second}_{name}.jpg"
-			imageio.imwrite(os.path.join(self.results_path, filename), processed_image)
-
-	def __run_algorithm(self, image, path):
-		'''Runs the algorithm in the image.
-
-		Parameters:
-			image: image filename.
-			path: image directory.
-
-		Returns the processed image.
-		'''
-
-		img = os.path.join(path, image)
-		alg = None
-
-		# UM (Unsharping Mask)
-		if self.algorithm == 'um':
-			alg = UM(img)
-		# CLAHE
-		if self.algorithm == 'clahe':
-			alg = CLAHE(img, self.results_path)
-		# HEF
-		if self.algorithm == 'hef':
-			alg = HEF(img, self.results_path)
-
-		try:
-			image = alg.run()
-		except Exception as e:
-			print(e)
-		else:
-			return image
+	def _export_run_info(self, runtime: float) -> None:
+		"""Exports the runtime and parameters of the current run to a text file in the results directory."""
+		with open(self._results_path / "runinfo.txt", 'w') as f:
+			f.write(f"Runtime: {timedelta(seconds=runtime)}\n")
+			f.write(f"Params:\n{json.dumps(self._params)}")
--- src/arguments.py
+++ src/arguments.py
@@ -1,40 +1,80 @@
 import argparse
+from typing import Any
+from pathlib import Path
+
 
 class ArgumentHandler:
-	'''Handles the program's arguments.'''
+	"""Handles the program's arguments."""
 
-	def __init__(self):
-		self.__parser = argparse.ArgumentParser()
-		self.__define_arguments()
-		self.__parsed_args = vars(self.__parser.parse_args())
+	__slots__ = ("_parser", "_parsed_args")
 
-	def __define_arguments(self):
-		group = self.__parser.add_mutually_exclusive_group(required=True)
-		group.add_argument("-i", metavar="img", type=str, help="image to be processed")
-		group.add_argument("-p", metavar="path", type=str, help="path to the images to be processed")
+	def __init__(self) -> None:
+		self._parser = argparse.ArgumentParser()
 
-		self.__parser.add_argument("-a", metavar="alg", type=str, help="algorithm to be used", required=True, choices=["clahe", "um", "hef"])
-		self.__parser.add_argument("-o", metavar="path", type=str, help="path to export the results", required=False)
+		self._define_arguments()
+		self._parsed_args = vars(self._parser.parse_args())
 
-	def get_image(self):
-		'''Gets the image from argument -i.'''
 
-		if self.__parsed_args['i']:
-			return self.__parsed_args['i']
+	def _define_arguments(self) -> None:
+		"""Defines the command-line arguments for the program."""
+		self._parser.add_argument("-i", "--input", type=Path, required=True, help="Path to the images to be processed")
+		self._parser.add_argument("-o", "--output", type=Path, required=True, help="Output path to export the results")
+		self._parser.add_argument("-a", "--algorithm", type=str, required=True, choices=["clahe", "um", "hef"], help="Algorithm to be used")
 
-	def get_path(self):
-		'''Gets the path from argument -p.'''
+		# Image processing options
+		self._parser.add_argument("--negate", action="store_true", help="Apply image negation before processing")
+		self._parser.add_argument("--shape", type=int, default=None, help="Output shape for the processed images, e.g., 640")
 
-		if self.__parsed_args['p']:
-			return self.__parsed_args['p']
+		# UM parameters
+		self._parser.add_argument("--filter-type", type=int, choices=[1, 2, 3, 4], default=None, help="[UM] Filter type (1=Gaussian, 2=Median, 3=Maximum, 4=Minimum)")
+		self._parser.add_argument("--radius", type=int, default=None, help="[UM] Radius (sigma) for Gaussian filter")
+		self._parser.add_argument("--amount", type=int, default=None, help="[UM] Sharpening amount")
 
-	def get_algorithm(self):
-		'''Gets the algorithm from argument -a.'''
+		# HEF parameters
+		self._parser.add_argument("--d0", type=int, default=None, help="[HEF] D0 value for high cut (1-90)")
 
-		return self.__parsed_args['a']
+		# CLAHE parameters
+		self._parser.add_argument("--window-size", type=int, default=None, help="[CLAHE] Window size")
+		self._parser.add_argument("--clip-limit", type=int, default=None, help="[CLAHE] Clip limit")
+		self._parser.add_argument("--n-iter", type=int, default=None, help="[CLAHE] Number of iterations")
+		self._parser.add_argument("--log", action="store_true", help="[CLAHE] Enable logging intermediate results for debugging")
 
-	def get_output_path(self):
-		'''Gets the path from argument -o.'''
 
-		if self.__parsed_args['o']:
-			return self.__parsed_args['o']
+	@property
+	def get_input_path(self) -> Path:
+		"""Returns provided input path."""
+		return self._parsed_args["input"]
+
+	@property
+	def get_output_path(self) -> Path:
+		"""Returns provided output path."""
+		return self._parsed_args["output"]
+
+	@property
+	def get_algorithm(self) -> str:
+		"""Returns provided algorithm."""
+		return self._parsed_args["algorithm"]
+
+	@property
+	def get_negate(self) -> bool:
+		"""Returns whether image negation should be applied."""
+		return self._parsed_args["negate"]
+
+	@property
+	def get_shape(self) -> int | None:
+		"""Returns provided output shape for the processed images."""
+		return self._parsed_args["shape"]
+
+	@property
+	def get_params(self) -> dict[str, Any]:
+		"""Returns algorithm-specific parameters."""
+		return {
+			"filter_type": self._parsed_args["filter_type"],
+			"radius": self._parsed_args["radius"],
+			"amount": self._parsed_args["amount"],
+			"d0": self._parsed_args["d0"],
+			"window_size": self._parsed_args["window_size"],
+			"clip_limit": self._parsed_args["clip_limit"],
+			"n_iter": self._parsed_args["n_iter"],
+			"log": self._parsed_args["log"]
+		}
